0d2e40f5348a6617813151b44bb8d9613f7d3be4
bashmakovartur <bashmakoff@inbox.ru>
2016-08-07 16:26:16 +0400

    Функция getMessage написана

--- build/js/check.js
+++ build/js/check.js
@@ -8,12 +8,17 @@ function getMessage(a, b) {
   } else if (typeof a == 'number') {
     return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + b * 4 + ' атрибутов';
   } else if (a instanceof Array) {
-    var amountOfRedPoints;
+    if (b instanceof Array) {
+      var artifactsSquare = 0;
+      for (x = 0; x < a.length; x++) {
+        artifactsSquare += a[x] * b [x];
+      }
+      return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
+    }
+    var amountOfRedPoints = 0;
     for (var i = 0; i < a.length; i++) {
-      amountOfRedPoints = a[i];
+      amountOfRedPoints += a[i];
     }
     return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
-  } else if ( a && b instanceof Array) {
-    return '111';
   }
 }
