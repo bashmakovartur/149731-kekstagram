c1b82c54fd66414aee7e649f753a5c9386badf19
bashmakovartur <bashmakoff@inbox.ru>
2016-08-14 23:21:59 +0400

    Валидация форм

--- src/upload.js
+++ src/upload.js
@@ -44,6 +44,7 @@
   var x = document.querySelector('#resize-x');
   var y = document.querySelector('#resize-y');
   var size = document.querySelector('#resize-size');
+	var fwd = document.getElementById('resize-fwd');
 
   x.min = 0;
   y.min = 0;
@@ -80,32 +81,26 @@
    * @return {boolean}
    */
   function resizeFormIsValid() {
-    if ((x.value + size.value > currentResizer._image.naturalWidth) || (y.value + size.value > currentResizer._image.naturalHeight)) {
-      document.getElementById('resize-fwd').disabled = true;
+    if ((x.value + size.value > currentResizer._image.naturalWidth) || (y.value + size.value > currentResizer._image.naturalHeight) || (x.value < 0) || (y.value < 0)) {
+      fwd.disabled = true;
       return false;
-    } else if ((x.value < 0) || (y.value < 0)) {
-      return false;
-    } else {
+    }	else {
+			fwd.disabled = false;
       return true;
     }
   }
 
-  x.onchange = function() {
-    if (resizeFormIsValid()) {
-      document.getElementById('resize-fwd').disabled = false;
-    }
+  x.oninput = function() {
+    resizeFormIsValid();
   };
-  y.onchange = function() {
-    if (resizeFormIsValid()) {
-      document.getElementById('resize-fwd').disabled = false;
-    }
+	
+  y.oninput = function() {
+    resizeFormIsValid();
   };
-  size.onchange = function() {
-    if (resizeFormIsValid()) {
-      document.getElementById('resize-fwd').disabled = false;
-    }
+	
+  size.oninput = function() {
+    resizeFormIsValid();
   };
-
 
   /**
    * Форма загрузки изображения.
