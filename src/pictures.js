28315b60235eea5e165aafa8b71035c53a2ea669
Bashmakov Artur <bashmakoff@inbox.ru>
2016-08-23 23:52:58 +0300

    Задание -Трансформируй это-

--- src/pictures.js
+++ src/pictures.js
@@ -13,7 +13,55 @@ var requestJSONP = function(address, fun) {
 
 var callBack = function(date) {
   pictures = date;
-  console.info(pictures);
+  // console.info(pictures);
+  pictures.forEach(function(picture) {
+    getPictureElement(picture, picturesContainer);
+  });
 };
 
 requestJSONP('http://localhost:1506/api/pictures', callBack);
+
+var picturesContainer = document.querySelector('.pictures');
+var filtersBlock = document.querySelector('.filters');
+filtersBlock.classList.add('hidden');
+var templateElement = document.querySelector('template');
+var elementToClone;
+var IMAGE_LOAD_TIMEOUT = 10000;
+
+if ('content' in templateElement) {
+  elementToClone = templateElement.content.querySelector('.picture');
+} else {
+  elementToClone = templateElement.querySelector('.picture');
+}
+
+var getPictureElement = function(date, container) {
+  var element = elementToClone.cloneNode(true);
+  element.querySelector('.picture-comments').textContent = date.comments;
+  element.querySelector('.picture-likes').textContent = date.likes;
+  element.getElementsByTagName('img')[0].src = date.url;
+  container.appendChild(element);
+
+  var photo = new Image(182, 182);
+  var photoLoadTimeout;
+
+  photo.onload = function() {
+    clearTimeout(photoLoadTimeout);
+    element.getElementsByTagName('img')[0].src = date.url;
+  };
+
+  photo.onerror = function() {
+    element.classList.add('picture-load-failure');
+  };
+
+  photo.src = date.url;
+
+  photoLoadTimeout = setTimeout(function() {
+    element.getElementsByTagName('img')[0].src = '';
+    element.classList.add('picture-load-failure');
+  }, IMAGE_LOAD_TIMEOUT);
+
+  return element;
+
+};
+
+filtersBlock.classList.remove('hidden');
